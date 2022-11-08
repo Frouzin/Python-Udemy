0996e3bcb465858222d97d75372fef3b699050b2
Frouzin <matheusereis44@gmail.com>
2022-11-08 15:45:04 -0300

    Codigo atividade IMC

--- exercicioIMC.py
+++ exercicioIMC.py
@@ -1,5 +1,18 @@
-name=(input('Digite seu nome:'))
-altura=(float(input('Digite sua altura:')))
-peso=(float(input('Digite seu peso:')))
+name=input('Digite seu nome: ')
+altura=(float(input('Digite sua altura: ')))
+peso=(float(input('Digite seu peso: ')))
 imc=(peso/(altura*altura))
-print(imc)
+
+print('{} tem {:.2f} M e pesa {} KG portanto seu imc é {:.1f}'.format(name,altura,peso,imc))
+
+if imc <= 18.5:
+    print('Você esta abaixo do peso ideal')
+elif imc >=18.5:
+    print('Você está no peso ideal')
+elif imc >= 25:
+    print('Você está com um sobre peso')
+elif imc >= 30 and imc < 40:
+    print('Você está Obeso!')
+else:
+    print('Você está com Obesidade Grave!')
+    
