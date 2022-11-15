55cb8c7ad56834e100df7bfb740afeb642417c2f
Frouzin <matheusereis44@gmail.com>
2022-11-15 18:38:57 -0300

    Novas atividades da UDEMY

--- exercicioIMC.py
+++ exercicioIMC.py
@@ -1,9 +1,9 @@
-name=input('Digite seu nome: ')
-altura=(float(input('Digite sua altura: ')))
-peso=(float(input('Digite seu peso: ')))
-imc=(peso/(altura*altura))
+name = input('Digite seu nome: ')
+altura = (float(input('Digite sua altura: ')))
+peso = (float(input('Digite seu peso: ')))
+imc = (peso/(altura*altura))
 
-print('{} tem {:.2f} M e pesa {} KG portanto seu imc é {:.1f}'.format(name,altura,peso,imc))
+print('{} tem {:.2f} M e pesa {} KG portanto seu imc é {:.1f}'.format(name, altura, peso, imc))
 
 if imc <= 18.5:
     print('Você esta abaixo do peso ideal')
