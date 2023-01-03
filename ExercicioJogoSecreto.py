2970e7048186671a8ee83a1554920503d6e4f0ab
Frouzin <matheusereis44@gmail.com>
2023-01-03 14:38:30 -0300

    projeto terminado

--- ExercicioJogoSecreto.py
+++ ExercicioJogoSecreto.py
@@ -1,10 +1,14 @@
 import random
+import os
 
 palavra_secreta = 'amor'
 letras_acertadas = ''
+numero_tentativa = 0
 
 while True:
+
     letra_digitada = input('Digite uma letra:')
+    numero_tentativa +=1
 
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra...')
@@ -13,12 +17,24 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
-            
+            palavra_formada += letra_secreta
         else:
-            print('*')
+            palavra_formada += '*'
+
+    print('Palavra formada: ',palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system('cls')
+        print('VOCÊ GANHOU')
+        print('A palavra era', palavra_formada)
+        print('Tentativas: ',numero_tentativa)
+        letras_acertadas = ''
+        numero_tentativa = 0
+        
+        
 
         # print(letras_acertadas)
     # print('Bora')
